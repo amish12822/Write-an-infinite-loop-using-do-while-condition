57cf57b2e57ffa64991f6c6f44c114d55bd1cc31
amish12822 <amishofficial23@gmail.com>
2024-07-29 17:53:16 +0530

    complete porgram create infinif loop with do-while loop

---
+++ infinify.java
@@ -0,0 +1,14 @@
+/**
+ * infinify
+ */
+public class infinify {
+
+    public static void main(String[] args) {
+        
+        int i = 1;
+        do {
+            System.out.println(i + " Hello! Amish");
+            i++;
+        } while ( i >= 1 );
+    }
+}
